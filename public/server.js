b68ae0e6ce1d74e0ab52745a5713522d42257438
Kenny Nyallau <k3nnywilliam@gmail.com>
2021-11-10 12:25:17 +0800

    Update static file directory for express server

--- public/server.js
+++ public/server.js
@@ -1,8 +1,15 @@
 let path = require('path');
 let express = require('express');
-let app = express();
+let app = express(), 
+          DIST_DIR=__dirname,
+          HTML_FILE = path.join(DIST_DIR, 'index.html');
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(DIST_DIR));
+
+app.get('*', (req, res) => {
+  res.sendFile(HTML_FILE)
+});
+
 app.set('port', process.env.PORT || 8080);
 
 let server = app.listen(app.get('port'), function() {
