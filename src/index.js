bbb39a1ac4ba37a03262d6b5e8d9fbc141356ca5
Kenny Nyallau <k3nnywilliam@gmail.com>
2021-11-10 12:23:55 +0800

    Add console log if mode is in development

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+  }
+
 ReactDOM.render(<App/>, document.getElementById('wrapper'));
